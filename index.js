0f767437d8b716e6310df9cf946ba13558ced0ea
thanhcongnguyen <nguyenthanhcong@unimarmores.com>
2018-06-25 01:31:38 +0700

    add  bin/www

--- index.js
+++ index.js
@@ -2,4 +2,42 @@ var express = require('express');
 var app = express();
 const PORT = 3000;
 
-app.listen(PORT, ()=> console.log(`server running with port${PORT}`));
+var routes = require('./routes/index');
+var auth = require('./routes/auth');
+
+app.use('/', routes);
+app.use('/login', auth);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+      res.status(err.status || 500);
+      res.render('error', {
+        message: err.message,
+        error: err
+      });
+    });
+}
+  
+  // production error handler
+  // no stacktraces leaked to user
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: {}
+    });
+});
+
+
+module.exports = app;
