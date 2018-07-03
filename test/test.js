9313cfe4670c36dc8fd0a421d0b7c5e858f3bb00
thanhcongnguyen <nguyenthanhcong@unimarmores.com>
2018-07-03 19:58:13 +0700

    add mochajs

---
+++ test/test.js
@@ -0,0 +1,26 @@
+var assert = require('assert');
+describe('Test case game Gomoku', function() {
+    it('1 Phe đủ 5 quân hàng ngang liền nhau là thắng', async function() {
+            
+    });
+    it("1 Phe đủ 5 quân hàng dọc liền nhau là thắng", async function(){
+
+    });
+
+    it("1 phe đủ 5 quân hàng chéo liền nhau là thắng", async function(){
+
+    });
+
+    it("Mỗi bên 1 lượt đánh chỉ 1 ô", async function(){
+
+    });
+
+    it("Lượt đánh đầu tiên Là X", async function(){
+
+    });
+    
+    it("1 ô không được đánh quá 1 lần", async function(){
+
+    });
+    
+});
